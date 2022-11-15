d53e243379d134431fd0be1107af7f9338ed53ac
daniiwi <118231099+daniiwi@users.noreply.github.com>
2022-11-14 19:44:46 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 module.exports = {
   token: process.env.TOKEN, // Your token here
-  guildId: "...", // Your guild id here
-  clientId: "...", // Your client id here
+  guildId: "", // Your guild id here
+  clientId: "1015374716590096464", // Your client id here
   prefix: "!", // Your bot prefix here
   version, // Your bot version here
 };
